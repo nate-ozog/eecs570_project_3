55fa6186190c553b8a2d9a20a5b1a2feae657133
Tim Dunn <me.timd1@gmail.com>
2020-04-11 20:17:07 -0400

    Untested full implementation of tiled kernel
     - finished work on kernel, too exhausted to start debugging now

--- xs_core.h
+++ xs_core.h
@@ -10,14 +10,18 @@ __global__ void xs_core_init(
   signed char mis_or_ind,
   uint8_t * mat
 ) {
+
   // Get the global thread index.
   uint32_t g_tx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
   // Initialize left column of backtrack matrix
   if (g_tx < qlen + 1)
     mat[g_tx*(tlen+1)] = INS;
+
   // Initialize top row of backtrack matrix
   if (g_tx < tlen + 1)
     mat[g_tx] = DEL;
+
   // Write 0 to the first cell of our transformed matrix row0.
   if (g_tx == 0)
 	mat[0] = 0;
@@ -31,7 +35,7 @@ __global__ void xs_core_comp(
 	uint32_t tlen,
 	uint32_t qlen,
 	int * col,
-	uint32_t kernel_col_offset,
+	uint32_t col_offset,
 	uint32_t max_strides,
 	signed char mis_or_ind,
 	uint8_t * mat
@@ -43,83 +47,77 @@ __global__ void xs_core_comp(
 	int * s_row2 = s_row1 + (blockDim.x+1)*max_strides;
 	int * s_temp = NULL;
 
-	//
-	uint32_t tid = threadIdx.x;
-	uint32_t kernel_comp_width = blockDim.x * 
+	// Shorthand thread index.
+	uint32_t tidx = threadIdx.x;
+
+	// Initialize Shared Memory (top two rows of sheared matrix).
+	if (tidx == 0) {
+		s_row0[0] = col[col_offset-1];
+		s_row1[0] = col[col_offset];
+		s_row1[1] = col_offset * mis_or_ind;
+	}
+	__syncthreads();
 
 	// Loop through every row that has useful work.
-	for (uint32_t row_idx = kernel_col_offset+1; row_idx < qlen+tlen+1; row_idx++) {
+	for (uint32_t row_idx = col_offset+1; row_idx < qlen+tlen+1; row_idx++) {
 
-		// Calculate band width based on row in sheared matrix.
-		uint32_t full_band_width;
-		if (row < 2) // no work
-			full_band_width = 0;
-		else if (row <= qlen)
-			full_band_width = row - 1;
-		else if (row <= tlen)
-			full_band_width = qlen;
+		// Calculate full sheared matrix band width based on row.
+		uint32_t band_width;
+		if (row_idx <= qlen)
+			band_width = row_idx - 1;
+		else if (row_idx <= tlen)
+			band_width = qlen;
 		else
-			full_band_width = qlen+tlen+1 - row;
+			band_width = qlen+tlen+1 - row_idx;
 
-		uint32_t local_band_width = 
-			min(full_band_width-kernel_col_offset, max_strides*blockDim.x);
+		// Calculate offset based on row in sheared matrix.
+		uint32_t row_offset = row_idx <= tlen ? 0 : row_idx-tlen;
+		uint32_t offset = row_offset + col_offset;
 
-		// Calculate offset based on row in sheared matrix
-		uint32_t row_offset = row <= tlen ? 0 : row-tlen;
-		uint32_t col_offset = row_offset + kernel_col_offset;
-
-		// Initialize Shared Memory
-		if (tid == 0) {
-			s_row0[0] = col[0];
-			s_row1[0] = col[1];
+		// Set Shared Memory (leftmost col and diag of current row).
+		if (tidx == 0) {
+			s_row2[0] = col[row_idx];
+			if (row_idx <= qlen) {
+				s_row2[row_idx-col_offset+1] = row_idx * mis_or_ind;
+			}
 		}
 
-		// Sh
-		for (uint32_t col_idx = 0; col_idx < local_band_width; col_idx += blockDim.x) {
+		// Stride across columns, taking offsets into account
+		for (uint32_t col_idx = 0; 
+				col_idx < band_width-col_offset+1 && 
+				col_idx < max_strides*blockDim.x; 
+				col_idx += blockDim.x) {
 
-			// Calculate global and local thread indices.
-			uint32_t gtidx = col_offset + col_idx + tid;
-			uint32_t ltidx = tid;
-
-			// Fill in SM at computed positions
-			if (tid < local_band_width)
-				s_row_up[l_tid+1] = row1[g_idx];
-			
-			// Write SM border element
-			if (l_tid == 0)
-				s_row_up[0] = row1[g_idx-1];
-
-			// If we need to write a border element on diagonal
-			if (g_tid == 0 && row <= qlen)
-				row2[row] = row * mis_or_ind;
-
-			// If we need to write a border element in left column
-			if (g_tid == 0 && row <= tlen)
-				row2[0] = row * mis_or_ind;
-
-			// Synchronize all threads, so that SM values are set.
-			__syncthreads();
+			// Calculate global and local indices for current thread.
+			uint32_t gidx = col_idx + offset + tidx;
+			uint32_t lidx = col_idx + tidx;
 
 			// Do the NW cell calculation.
-			if (g_tid < g_nthreads) {
-			int match = row0[g_idx-1] + cuda_nw_get_sim(q[g_idx-1], t[row-g_idx-1]);
-			int del = s_row_up[l_tid+1] + mis_or_ind;
-			int ins = s_row_up[l_tid] + mis_or_ind;
+			if (gidx < band_width) {
+				int match = s_row0[lidx-1] + cuda_nw_get_sim(q[gidx-1], t[row_idx-gidx-1]);
+				int del = s_row1[lidx] + mis_or_ind;
+				int ins = s_row1[lidx-1] + mis_or_ind;
 
-			// Write back to our current sliding window row index, set pointer.
-			int mat_idx = row-g_idx + g_idx*(tlen+1);
-			if (match >= ins && match >= del) {
-				row2[g_idx] = match;
-				mat[mat_idx] = MATCH;
-			}
-			else if (ins >= match && ins >= del) {
-				row2[g_idx] = ins;
-				mat[mat_idx] = INS;
-			}
-			else {
-				row2[g_idx] = del;
-				mat[mat_idx] = DEL;
-			}
+				// Write back to our current sliding window row index, set pointer.
+				int mat_idx = row_idx-gidx + gidx*(tlen+1);
+				if (match >= ins && match >= del) {
+					s_row2[lidx] = match;
+					mat[mat_idx] = MATCH;
+					if (tidx == blockIdx.x - 1)
+						col[row_idx] = match;
+				}
+				else if (ins >= match && ins >= del) {
+					s_row2[lidx] = ins;
+					mat[mat_idx] = INS;
+					if (tidx == blockIdx.x - 1)
+						col[row_idx] = ins;
+				}
+				else {
+					s_row2[lidx] = del;
+					mat[mat_idx] = DEL;
+					if (tidx == blockIdx.x - 1)
+						col[row_idx] = del;
+				}
 			}
 		}
 
@@ -128,6 +126,7 @@ __global__ void xs_core_comp(
 		s_row0 = s_row1;
 		s_row1 = s_row2;
 		s_row2 = s_temp;
+		__syncthreads();
 	}
 }
 
