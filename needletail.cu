c9370d55db2e67e77951d3860fe4d2f2e2ab562f
Nathan A Ozog <ozog@umich.edu>
2020-04-12 11:33:22 -0400

    Next-gen batching done, functionally correct.

--- needletail.cu
+++ needletail.cu
@@ -4,7 +4,6 @@
 #include <pthread.h>
 
 TestBatch test_batch("batch.txt");
-volatile uint32_t oustanding_batches = 0;
 
 // Reads in the similarity matrix file into GPU constant memory.
 signed char * init_similarity_matrix() {
@@ -21,6 +20,31 @@ signed char * init_similarity_matrix() {
   return s;
 }
 
+// Worker thread function.
+void * worker(void * arg) {
+  Test_t tests[STREAM_BATCH_SIZE];
+  bool swap_t_q[STREAM_BATCH_SIZE];
+  uint32_t batch_cnt = 0;
+  // Loop until all tests are done.
+  while (test_batch.next_test(tests[batch_cnt])) {
+    swap_t_q[batch_cnt] = tests[batch_cnt].s2_len > tests[batch_cnt].s1_len;
+    if (swap_t_q[batch_cnt]) {
+      std::swap(tests[batch_cnt].s1, tests[batch_cnt].s2);
+      std::swap(tests[batch_cnt].s1_len, tests[batch_cnt].s2_len);
+    }
+    ++batch_cnt;
+    // If we have a batch ready to go.
+    if (batch_cnt == STREAM_BATCH_SIZE) {
+      needletail_stream_batch(batch_cnt, swap_t_q, tests);
+      batch_cnt = 0;
+    }
+  }
+  // If we ran out of tests but still have not a full batch ready.
+  if (batch_cnt != 0)
+    needletail_stream_batch(batch_cnt, swap_t_q, tests);
+  return NULL;
+}
+
 // Driver.
 int main() {
   // Setup system.
@@ -29,23 +53,17 @@ int main() {
   test_batch.set_gapscore(GAP_SCORE);
   delete [] sim_mat;
 
+  pthread_t t[NUM_THREADS];
   auto start = std::chrono::high_resolution_clock::now();
-  Test_t new_test;
-  Test_t ** tests = new Test_t * [STREAM_BATCH_SIZE];
-  uint32_t batch_cnt = 0;
-  while (test_batch.next_test(new_test)) {
-    tests[batch_cnt] = new Test_t;
-    *tests[batch_cnt] = new_test;
-    ++batch_cnt;
-    if (batch_cnt == STREAM_BATCH_SIZE) {
-      while (oustanding_batches >= MAX_OUSTANDING_BATCHES);
-      needletail_stream_batch(tests);
-      batch_cnt = 0;
-    }
-  }
-  while (oustanding_batches);
+  // Launch the worker threads
+  for (uint32_t i = 0; i < NUM_THREADS; i++)
+    pthread_create(&t[i], NULL, worker, NULL);
+  // Join the worker threads
+  for (uint32_t i = 0; i < NUM_THREADS; i++)
+    pthread_join(t[i], NULL);
   auto finish = std::chrono::high_resolution_clock::now();
 
+  // Write results and terminate.
   auto total_runtime = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
   test_batch.set_time(total_runtime.count());
   test_batch.save_results("GPU_results.txt");
--- needletail_threads.cuh
+++ needletail_threads.cuh
@@ -6,6 +6,8 @@
 #include "cuda_error_check.cuh"
 #include "needletail_kernels.cuh"
 
+std::mutex mtx;
+
 struct stream_args {
   void * mem;
   uint8_t * mat;
@@ -17,104 +19,66 @@ struct stream_args {
   char * q_d;
 };
 
-struct d2h_staller_args {
-  stream_args ** s_args;
-  cudaStream_t ** s;
-  Test_t ** tests;
-};
-
-struct bt_worker_args {
-  Test_t * test;
-  uint8_t * mat;
-};
-
-void * bt_worker (void * arg) {
-  struct bt_worker_args * args = (struct bt_worker_args *) arg;
-  Test_t * test = args->test;
-  uint8_t * mat = args->mat;
-  std::pair<char *, char *> algn = nw_ptr_backtrack(mat, 0, test->s1, test->s2, test->s1_len, test->s2_len);
-  test_batch.log_result(test->id, algn.first, algn.second, 0, 0);
-  delete [] algn.first;
-  delete [] algn.second;
-  cudaFreeHost(mat);
-  delete test;
-  __atomic_sub_fetch(&oustanding_batches, 1, __ATOMIC_RELAXED);
-  pthread_exit(NULL);
-}
-
-void * d2h_staller (void * arg) {
-  d2h_staller_args * args = (struct d2h_staller_args *) arg;
-  for (size_t i = 0; i < STREAM_BATCH_SIZE; ++i) {
-    cudaStreamSynchronize(*args->s[i]);
-    cudaFree(args->s_args[i]->mat);
-    cudaStreamDestroy(*args->s[i]);
-    pthread_t bt_worker_thread;
-    bt_worker_args * bt_args = new bt_worker_args;
-    bt_args->test = args->tests[i];
-    bt_args->mat = args->s_args[i]->mat;
-    delete args->s[i];
-    delete args->s_args[i];
-    pthread_create(&bt_worker_thread, NULL, bt_worker, bt_args);
-    pthread_detach(bt_worker_thread);
-  }
-  delete args;
-  pthread_exit(NULL);
-}
-
 void needletail_stream_batch (
-  Test_t ** tests
+  uint32_t batch_size,
+  bool * swap_t_q,
+  Test_t * tests
 ) {
 
-  // Create an array of stream arguments and stream pointers.
-  stream_args ** s_args = new stream_args * [STREAM_BATCH_SIZE];
-  cudaStream_t ** s = new cudaStream_t * [STREAM_BATCH_SIZE];
+  // Create an array of stream arguments and streams.
+  stream_args s_args[batch_size];
+  cudaStream_t s[batch_size];
 
-  // Create the streams.
-  for (size_t i = 0; i < STREAM_BATCH_SIZE; ++i) {
-    s[i] = new cudaStream_t;
-    cudaStreamCreate(s[i]);
-  }
+  // Grab the scheduling lock.
+  mtx.lock();
 
-  // Claim GPU memory and setup arguments.
-  for (size_t i = 0; i < STREAM_BATCH_SIZE; ++i) {
-    uint32_t tlen = tests[i]->s1_len;
-    uint32_t qlen = tests[i]->s2_len;
+  // Create the streams, claim GPU memory, and setup arguments.
+  for (size_t i = 0; i < batch_size; ++i) {
+    cudaStreamCreate(&s[i]);
+    uint32_t tlen = tests[i].s1_len;
+    uint32_t qlen = tests[i].s2_len;
     uint64_t num_GPU_mem_bytes = 3 * (tlen + 1) * sizeof(int);
     num_GPU_mem_bytes += (tlen + 1) * (qlen + 1) * sizeof(uint8_t);
     num_GPU_mem_bytes += tlen * sizeof(char);
     num_GPU_mem_bytes += qlen * sizeof(char);
-    s_args[i] = new stream_args;
-    cudaMalloc((void **) & s_args[i]->mem, num_GPU_mem_bytes);
-    cudaHostAlloc((void**)& s_args[i]->mat, (tlen+1) * (qlen+1) * sizeof(uint8_t), cudaHostAllocDefault);
-    s_args[i]->row0_d = (int *) s_args[i]->mem;
-    s_args[i]->row1_d = s_args[i]->row0_d + (tlen + 1);
-    s_args[i]->row2_d = s_args[i]->row1_d + (tlen + 1);
-    s_args[i]->mat_d = (uint8_t *) (s_args[i]->row2_d + (tlen + 1));
-    s_args[i]->t_d = (char *) (s_args[i]->mat_d + (tlen + 1) * (qlen + 1));
-    s_args[i]->q_d = s_args[i]->t_d + tlen;
+    size_t free = 0, total = 0;
+    while (free < 2 * num_GPU_mem_bytes)
+      cudaMemGetInfo(&free, &total);
+    cudaMalloc((void **) & s_args[i].mem, num_GPU_mem_bytes);
+    cudaHostAlloc((void**)& s_args[i].mat, (tlen+1) * (qlen+1) * sizeof(uint8_t), cudaHostAllocDefault);
+    s_args[i].row0_d = (int *) s_args[i].mem;
+    s_args[i].row1_d = s_args[i].row0_d + (tlen + 1);
+    s_args[i].row2_d = s_args[i].row1_d + (tlen + 1);
+    s_args[i].mat_d = (uint8_t *) (s_args[i].row2_d + (tlen + 1));
+    s_args[i].t_d = (char *) (s_args[i].mat_d + (tlen + 1) * (qlen + 1));
+    s_args[i].q_d = s_args[i].t_d + tlen;
   }
 
   // Launch depth first kernel streams.
-  for (size_t i = 0; i < STREAM_BATCH_SIZE; ++i) {
-    cudaMemcpyAsync(s_args[i]->t_d, tests[i]->s1, tests[i]->s1_len * sizeof(char), cudaMemcpyHostToDevice, *s[i]);
-    cudaMemcpyAsync(s_args[i]->q_d, tests[i]->s2, tests[i]->s2_len * sizeof(char), cudaMemcpyHostToDevice, *s[i]);
-    needletail_init_kernel <<< divide_then_round_up((tests[i]->s1_len + 1), BLOCK_SIZE), BLOCK_SIZE, 0, *s[i] >>>
-        (tests[i]->s1_len, tests[i]->s2_len, GAP_SCORE, s_args[i]->row0_d, s_args[i]->row1_d, s_args[i]->mat_d);
-    needletail_comp_kernel <<< 1, BLOCK_SIZE, 0, *s[i] >>>
-      (s_args[i]->t_d, s_args[i]->q_d, tests[i]->s1_len, tests[i]->s2_len, GAP_SCORE,
-        s_args[i]->row0_d, s_args[i]->row1_d, s_args[i]->row2_d, s_args[i]->mat_d);
-    cudaMemcpyAsync(s_args[i]->mat, s_args[i]->mat_d, (tests[i]->s1_len + 1) * (tests[i]->s2_len + 1) * sizeof(uint8_t), cudaMemcpyDeviceToHost, *s[i]);
+  for (size_t i = 0; i < batch_size; ++i) {
+    cudaMemcpyAsync(s_args[i].t_d, tests[i].s1, tests[i].s1_len * sizeof(char), cudaMemcpyHostToDevice, s[i]);
+    cudaMemcpyAsync(s_args[i].q_d, tests[i].s2, tests[i].s2_len * sizeof(char), cudaMemcpyHostToDevice, s[i]);
+    needletail_init_kernel <<< divide_then_round_up((tests[i].s1_len + 1), BLOCK_SIZE), BLOCK_SIZE, 0, s[i] >>>
+        (tests[i].s1_len, tests[i].s2_len, GAP_SCORE, s_args[i].row0_d, s_args[i].row1_d, s_args[i].mat_d);
+    needletail_comp_kernel <<< 1, BLOCK_SIZE, 0, s[i] >>>
+      (s_args[i].t_d, s_args[i].q_d, tests[i].s1_len, tests[i].s2_len, GAP_SCORE,
+        s_args[i].row0_d, s_args[i].row1_d, s_args[i].row2_d, s_args[i].mat_d);
+    cudaMemcpyAsync(s_args[i].mat, s_args[i].mat_d, (tests[i].s1_len + 1) * (tests[i].s2_len + 1) * sizeof(uint8_t), cudaMemcpyDeviceToHost, s[i]);
   }
 
-  // Peel off a thread to handle stream completion.
-  pthread_t d2h_staller_thread;
-  d2h_staller_args * d2h_args = new d2h_staller_args;
-  d2h_args->s_args = s_args;
-  d2h_args->s = s;
-  d2h_args->tests = tests;
-  pthread_create(&d2h_staller_thread, NULL, d2h_staller, d2h_args);
-  pthread_detach(d2h_staller_thread);
-  oustanding_batches += STREAM_BATCH_SIZE;
+  // Release the scheduling lock.
+  mtx.unlock();
+
+  for (size_t i = 0; i < batch_size; ++i) {
+    cudaStreamSynchronize(s[i]);
+    cudaFree(s_args[i].mem);
+    cudaStreamDestroy(s[i]);
+    std::pair<char *, char *> algn = nw_ptr_backtrack(s_args[i].mat,
+      swap_t_q[i], tests[i].s1, tests[i].s2, tests[i].s1_len, tests[i].s2_len);
+    cudaFreeHost(s_args[i].mat);
+    test_batch.log_result(tests[i].id, algn.first, algn.second, 0, 0);
+  }
+
   return;
 }
 
