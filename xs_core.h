83ce973f69e6346624ef00e35438258600438702
Tim Dunn <me.timd1@gmail.com>
2020-04-12 22:37:21 -0400

    Pulled column loop iteration into the kernel

--- xs_core.h
+++ xs_core.h
@@ -35,7 +35,6 @@ __global__ void xs_core_comp(
 	uint32_t tlen,
 	uint32_t qlen,
 	int * col,
-	uint32_t col_offset,
 	uint32_t max_strides,
 	signed char mis_or_ind,
 	uint8_t * mat
@@ -51,73 +50,76 @@ __global__ void xs_core_comp(
 	int * s_row2 = s_row1 + (bdim+1)*max_strides;
 	int * s_temp = NULL;
 
-	// Initialize Shared Memory (top two rows of sheared matrix section).
-	if (tidx == 0) {
-		s_row0[0] = (col_offset-1) * mis_or_ind;// on diag or from previous kernel
-		s_row1[0] = col[col_offset];		 	// passed from previous kernel
-		s_row1[1] = col_offset * mis_or_ind; 	// always on diag
-	}
-	__syncthreads();
+	for (uint32_t col_offset = 1; col_offset < qlen+1; col_offset+=max_strides*bdim) {
 
-	// Loop through every row that has useful work.
-	for (uint32_t row_idx = col_offset+1; row_idx < qlen+tlen+1; row_idx++) {
-
-		// Calculate row offset, due to diagonalization past row tlen.
-		uint32_t row_offset = row_idx <= tlen ? 0 : row_idx - (tlen+1);
-
-		// Calculate last column computed by kernel (local and global).
-		uint32_t last_gcol = min(qlen, col_offset+max_strides*bdim-1);
-		uint32_t last_lcol = min(max_strides*bdim-1, last_gcol-col_offset);
-
-		// Set Shared Memory values for leftmost column and diagonal
+		// Initialize Shared Memory (top two rows of sheared matrix section).
 		if (tidx == 0) {
-			s_row2[0] = col[row_idx];
-			if (row_idx <= last_gcol)
-				s_row2[row_idx-col_offset+1] = row_idx * mis_or_ind;
+			s_row0[0] = (col_offset-1) * mis_or_ind;// on diag or from previous kernel
+			s_row1[0] = col[col_offset];		 	// passed from previous kernel
+			s_row1[1] = col_offset * mis_or_ind; 	// always on diag
 		}
+		__syncthreads();
 
-		// Stride across columns.
-		for (uint32_t col_idx = 0; col_idx <= last_lcol; col_idx += bdim) {
+		// Loop through every row that has useful work.
+		for (uint32_t row_idx = col_offset+1; row_idx < qlen+tlen+1; row_idx++) {
 
-			// Calculate global and local indices for current thread.
-			uint32_t gidx = col_offset + col_idx + tidx;
-			uint32_t lidx = col_idx + tidx;
+			// Calculate row offset, due to diagonalization past row tlen.
+			uint32_t row_offset = row_idx <= tlen ? 0 : row_idx - (tlen+1);
 
-			// Do the NW cell calculation.
-			if (gidx > row_offset && gidx <= last_gcol && gidx < row_idx) {
-				int match = s_row0[lidx] + cuda_nw_get_sim(q[gidx-1], t[row_idx-gidx-1]);
-				int del = s_row1[lidx+1] + mis_or_ind;
-				int ins = s_row1[lidx] + mis_or_ind;
+			// Calculate last column computed by kernel (local and global).
+			uint32_t last_gcol = min(qlen, col_offset+max_strides*bdim-1);
+			uint32_t last_lcol = min(max_strides*bdim-1, last_gcol-col_offset);
 
-				// Write back to our current sliding window row index, set pointer.
-				int mat_idx = row_idx-gidx + gidx*(tlen+1);
-				if (match >= ins && match >= del) {
-					s_row2[lidx+1] = match;
-					mat[mat_idx] = MATCH;
-					if (lidx == last_lcol)
-						col[row_idx] = match;
-				}
-				else if (ins >= match && ins >= del) {
-					s_row2[lidx+1] = ins;
-					mat[mat_idx] = INS;
-					if (lidx == last_lcol)
-						col[row_idx] = ins;
-				}
-				else {
-					s_row2[lidx+1] = del;
-					mat[mat_idx] = DEL;
-					if (lidx == last_lcol)
-						col[row_idx] = del;
+			// Set Shared Memory values for leftmost column and diagonal
+			if (tidx == 0) {
+				s_row2[0] = col[row_idx];
+				if (row_idx <= last_gcol)
+					s_row2[row_idx-col_offset+1] = row_idx * mis_or_ind;
+			}
+
+			// Stride across columns.
+			for (uint32_t col_idx = 0; col_idx <= last_lcol; col_idx += bdim) {
+
+				// Calculate global and local indices for current thread.
+				uint32_t gidx = col_offset + col_idx + tidx;
+				uint32_t lidx = col_idx + tidx;
+
+				// Do the NW cell calculation.
+				if (gidx > row_offset && gidx <= last_gcol && gidx < row_idx) {
+					int match = s_row0[lidx] + cuda_nw_get_sim(q[gidx-1], t[row_idx-gidx-1]);
+					int del = s_row1[lidx+1] + mis_or_ind;
+					int ins = s_row1[lidx] + mis_or_ind;
+
+					// Write back to our current sliding window row index, set pointer.
+					int mat_idx = row_idx-gidx + gidx*(tlen+1);
+					if (match >= ins && match >= del) {
+						s_row2[lidx+1] = match;
+						mat[mat_idx] = MATCH;
+						if (lidx == last_lcol)
+							col[row_idx] = match;
+					}
+					else if (ins >= match && ins >= del) {
+						s_row2[lidx+1] = ins;
+						mat[mat_idx] = INS;
+						if (lidx == last_lcol)
+							col[row_idx] = ins;
+					}
+					else {
+						s_row2[lidx+1] = del;
+						mat[mat_idx] = DEL;
+						if (lidx == last_lcol)
+							col[row_idx] = del;
+					}
 				}
 			}
-		}
 
-		// Shift sliding window.
-		s_temp = s_row0;
-		s_row0 = s_row1;
-		s_row1 = s_row2;
-		s_row2 = s_temp;
-		__syncthreads();
+			// Shift sliding window.
+			s_temp = s_row0;
+			s_row0 = s_row1;
+			s_row1 = s_row2;
+			s_row2 = s_temp;
+			__syncthreads();
+		}
 	}
 }
 
@@ -161,12 +163,10 @@ uint8_t * xs_t_geq_q_man(
   uint32_t block_size = 1024;
   uint32_t max_strides = 3; // 49152 / (1025*4*3) = 3.996
 
-  // Loop through every wavefront/diagonal.
-  for (uint32_t col_idx = 1; col_idx < qlen+1; col_idx+=max_strides*block_size) {
-	uint32_t shared_mem_size = (block_size+1) * sizeof(int) * max_strides * 3;
-    xs_core_comp <<< 1, block_size, shared_mem_size, *stream >>>
-      (t_d, q_d, tlen, qlen, col_d, col_idx, max_strides, mis_or_ind, mat_d);
-  }
+  // Call our kernel.
+  uint32_t shared_mem_size = (block_size+1) * sizeof(int) * max_strides * 3;
+  xs_core_comp <<< 1, block_size, shared_mem_size, *stream >>>
+    (t_d, q_d, tlen, qlen, col_d, max_strides, mis_or_ind, mat_d);
 
   // Allocate pinned memory on the host for faster data transfer.
   uint8_t* mat;
